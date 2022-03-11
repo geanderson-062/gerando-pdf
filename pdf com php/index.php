591a4077f621dfe30c1580f6bba9f17eb6423511
geanderson062 <geanderson062@outlook.com>
2022-03-11 13:51:41 -0300

    11/03/2022
    gerando pdf com php com lib dompdf

---
+++ pdf com php/index.php
@@ -0,0 +1,34 @@
+<?php	
+
+	//referenciar o DomPDF com namespace
+	use Dompdf\Dompdf;
+
+	// include autoloader
+	require_once("dompdf/autoload.inc.php");
+
+	//Criando a Instancia
+	$dompdf = new DOMPDF();
+
+	// Carrega seu HTML
+	$dompdf->load_html('
+
+	<!--inicio-->
+	
+			<h1 style="text-align: center;">Gerar PDF</h1>
+			<p>Sistema para gerar pdf com a lib dompdf</p>
+
+	<!--fim-->
+		
+			');
+
+	//Renderizar o html
+	$dompdf->render();
+
+	//Exibibir a página
+	$dompdf->stream(
+		"relatorio_celke.pdf", 
+		array(
+			"Attachment" => false //Para realizar o download somente alterar para true
+		)
+	);
+?>
